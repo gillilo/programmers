713afd16b4583dc1871d660c125367e2d3ef7763
gillilo <ghlwns9258@gmail.com>
2024-10-15 00:12:57 +0900

    1

---
+++ src/pccp9/Solution.java
@@ -0,0 +1,13 @@
+package pccp9;
+
+public class Solution {
+    public int solution(int[] wallet, int[] bill) {
+        int ww = wallet[0];
+        int wh = wallet[1];
+        int bw = bill[0];
+        int bh = bill[1];
+
+        int answer = 0;
+        return answer;
+    }
+}
